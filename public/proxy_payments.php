9e36891674a65e593c3fc20925975c76008ad4cc
manuelgonzalez18-ui <manuelgonzalez18@gmail.com>
2026-09-07 09:15:28 -0400

    Use payment audit library from portal payment flow

--- public/proxy_payments.php
+++ public/proxy_payments.php
@@ -9,7 +9,7 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type');
 
-require_once __DIR__ . '/payment_audit.php';
+require_once __DIR__ . '/payment_audit_lib.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(204);
